fecbbc00948f5f9273f1b7d448c409ed54f9fffc
mabt <tuoyomabiaku@yahoo.com>
2020-08-12 10:57:00 +0100

    changed Featured to Completed

--- main/app/Modules/BasicSite/Resources/assets/js/components/partials/home/OurProjects.vue
+++ main/app/Modules/BasicSite/Resources/assets/js/components/partials/home/OurProjects.vue
@@ -8,7 +8,7 @@
           <div class="tg-sectionhead">
             <div class="tg-sectiontitle">
               <!-- <h2>some of our</h2> -->
-              <h3>Featured Projects</h3>
+              <h3>Completed Projects</h3>
             </div>
             <div class="tg-description">
               <p>
